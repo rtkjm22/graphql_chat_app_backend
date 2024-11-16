dc8c8e88c3116d7c3d3f4429a9679d89ac4f01ab
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2024-11-16 14:57:07 +0900

    userクラスの更新処理追加

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [UserService, UserResolver]
+  providers: [UserService, UserResolver, JwtService, PrismaService],
 })
 export class UserModule {}
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,38 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/graphql-auth.guard';
+import { User } from './user.type';
+import * as GraphQLUpload from 'graphql-upload-ts/dist/Upload';
+import { Request } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { join } from 'path';
+import { createWriteStream } from 'fs';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(GraphqlAuthGuard)
+  @Mutation(() => User)
+  async updateProfile(
+    @Args('fullname') fullname: string,
+    @Args('file', { type: () => GraphQLUpload, nullable: true })
+    file: GraphQLUpload.FileUpload,
+    @Context() context: { req: Request },
+  ) {
+    const imageUrl = file ? await this.storeImageAndGetUrl(file) : null;
+    const userId = context.req.user.sub;
+    return this.userService.updateProfile(userId, fullname, imageUrl);
+  }
+
+  private async storeImageAndGetUrl(file: GraphQLUpload.FileUpload) {
+    const { createReadStream, filename } = await file;
+    const uniqueFilename = `${uuidv4()}_${filename}`;
+    const imagePath = join(process.cwd(), 'public', uniqueFilename);
+    const imageUrl = `${process.env.APP_URL}/${uniqueFilename}`;
+    const readStream = createReadStream();
+    readStream.pipe(createWriteStream(imagePath));
+    return imageUrl
+  }
+}
